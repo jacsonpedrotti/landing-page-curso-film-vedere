2f55722cbc1e12c553f3444e825713a77f6d97bd
Jacson Pedrotti <pedrottijacson@gmail.com>
2025-10-07 15:00:37 -0300

    alteração mensagem personalizada icone whatsapp

--- src/components/WhatsappSupport.jsx
+++ src/components/WhatsappSupport.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const WHATS_URL = 'https://api.whatsapp.com/send/?phone=5548996999796'
+const MESSAGE = encodeURIComponent('Olá, tenho dúvidas sobre o Curso aplicação de Películas, poderia me ajudar?')
+const WHATS_URL = `https://api.whatsapp.com/send/?phone=5548996999796&text=${MESSAGE}`
 
 function WhatsappSupport() {
   return (
